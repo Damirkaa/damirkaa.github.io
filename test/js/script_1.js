3cec6f7c65feed0d1a51270623706d2550007e1b
Damirkaa <damirkagat@yandex.ru>
2017-03-26 22:24:33 +0300

    test
    test

--- test/js/script_1.js
+++ test/js/script_1.js
@@ -23,3 +23,168 @@ else{
 	$("#menu_main").css("display","none");
 }
 });  
+
+$("#hide").click(function(){
+	var $part1=$(".right").width();
+	if ($part1=="485") {
+		$(".right").animate({width:'50px'},900);
+		$(".h1").hide();
+		$(".h2").hide();
+		$(".discription").hide();
+		$("#slider").hide();
+		$("#carouselLeft").hide();
+		$("#carouselRight").hide();
+		$(".time").hide();
+		$(".stars").hide();
+		$(".h3").animate({opacity:'1'},900);
+		$("#food").animate({marginLeft:'0'},900);
+	}
+	else{
+		$(".right").animate({width:'485px'},900,function(){$(".time").delay(900).show();});
+		$(".h1").show();
+		$(".h2").show();
+		$(".discription").show();
+		$("#slider").show();
+		$("#carouselLeft").show();
+		$("#carouselRight").show();
+		$(".stars").show();
+		$(".h3").animate({opacity:'0'},900)
+		$("#food").animate({marginLeft:'-40%'},900);
+	}
+});
+
+$(document).ready(function (){
+	var $st1=$("#star1");
+	var $st2=$("#star2");
+	var $st3=$("#star3");
+	var $st4=$("#star4");
+	var $st5=$("#star5");
+	var mark;
+
+	$($st1.hover(
+		function(){
+		$st1.css("color","#FFD700");
+		},
+		function(){
+		$st1.css("color","");
+		}
+	))
+
+	$($st2.hover(
+		function(){
+		$st1.css("color","#FFD700");
+		$st2.css("color","#FFD700");
+		},
+		function(){
+		$st1.css("color","");
+		$st2.css("color","");
+		}
+	))
+
+	$($st3.hover(
+		function(){
+		$st1.css("color","#FFD700");
+		$st2.css("color","#FFD700");
+		$st3.css("color","#FFD700");
+		},
+		function(){
+		$st1.css("color","");
+		$st2.css("color","");
+		$st3.css("color","");
+		}
+	))
+
+	$($st4.hover(
+		function(){
+		$st1.css("color","#FFD700");
+		$st2.css("color","#FFD700");
+		$st3.css("color","#FFD700");
+		$st4.css("color","#FFD700");
+		},
+		function(){
+		$st1.css("color","");
+		$st2.css("color","");
+		$st3.css("color","");
+		$st4.css("color","");
+		}
+	))
+
+	$($st5.hover(
+		function(){
+		$st1.css("color","#FFD700");
+		$st2.css("color","#FFD700");
+		$st3.css("color","#FFD700");
+		$st4.css("color","#FFD700");
+		$st5.css("color","#FFD700");
+		},
+		function(){
+		$st1.css("color","");
+		$st2.css("color","");
+		$st3.css("color","");
+		$st4.css("color","");
+		$st5.css("color","");
+		}
+	))
+
+	$st1.click(function(){
+		$st1.unbind();
+		$st2.unbind();
+		$st3.unbind();
+		$st4.unbind();
+		$st5.unbind();
+		$st1.css("color","#FFD700");
+		mark=1;
+	})
+
+	$st2.click(function(){
+		$st1.unbind();
+		$st2.unbind();
+		$st3.unbind();
+		$st4.unbind();
+		$st5.unbind();
+		$st1.css("color","#FFD700");
+		$st2.css("color","#FFD700");
+		mark=2;
+	})
+
+	$st3.click(function(){
+		$st1.unbind();
+		$st2.unbind();
+		$st3.unbind();
+		$st4.unbind();
+		$st5.unbind();
+		$st1.css("color","#FFD700");
+		$st2.css("color","#FFD700");
+		$st3.css("color","#FFD700");
+		mark=3;
+	})
+
+	$st4.click(function(){
+		$st1.unbind();
+		$st2.unbind();
+		$st3.unbind();
+		$st4.unbind();
+		$st5.unbind();
+		$st1.css("color","#FFD700");
+		$st2.css("color","#FFD700");
+		$st3.css("color","#FFD700");
+		$st4.css("color","#FFD700");
+		mark=4;
+	})
+
+	$st5.click(function(){
+		$st1.unbind();
+		$st2.unbind();
+		$st3.unbind();
+		$st4.unbind();
+		$st5.unbind();
+		$st1.css("color","#FFD700");
+		$st2.css("color","#FFD700");
+		$st3.css("color","#FFD700");
+		$st4.css("color","#FFD700");
+		$st5.css("color","#FFD700");
+		mark=5;
+	})
+});
+
+
