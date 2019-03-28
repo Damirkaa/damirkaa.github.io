acdc4feec0e64982be1f654fa547b52813348372
damir <damirkagat@yandex.ru>
2019-03-28 22:55:04 +0300

    Test app

---
+++ test-app/precache-manifest.5555312196f1b4046ef0615cbfda354b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8e29996faf81b264ce0",
+    "url": "/static/css/main.d09d6f70.chunk.css"
+  },
+  {
+    "revision": "e8e29996faf81b264ce0",
+    "url": "/static/js/main.02e89105.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aa5882d9a03ca2106952",
+    "url": "/static/js/2.90bddcb8.chunk.js"
+  },
+  {
+    "revision": "bedcb921ac005d55d85b7947589b4e9b",
+    "url": "/index.html"
+  }
+];
